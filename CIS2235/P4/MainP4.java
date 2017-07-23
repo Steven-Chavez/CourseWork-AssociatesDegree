71f7e72dcb8e765e2563975a54e59db9906e30cd
Steven Chavez <schavez256@yahoo.com>
2017-07-22 23:27:18 -0600

    Added Main.java to project 4

---
+++ CIS2235/P4/MainP4.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+
+/**
+ * Entry point for ChavezP4 program 
+ *
+ * File: MainP4.java
+ * Program 4: Color Builder Frame
+ * CIS2235
+ **/ 
+ public class MainP4
+ { 
+	 private ColorFrame frame;
+	 
+	public static void main(String[] args)
+	{
+		MainP4 app = new MainP4();
+	}
+	
+	MainP4()
+	{
+		 frame = new ColorFrame();
+		 frame.setTitle("ChavezP2: Color Builder");
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 frame.setSize(400,600);
+		 frame.setVisible(true);
+	}
+ }
