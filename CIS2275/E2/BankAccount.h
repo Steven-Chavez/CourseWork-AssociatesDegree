1bf7d3d8288d8568696a329037ce4c8bd72bf921
Steven Chavez <schavez256@yahoo.com>
2017-08-04 21:28:49 -0600

    Added BankAccount.h to Exercise 2

---
+++ CIS2275/E2/BankAccount.h
@@ -0,0 +1,30 @@
+//Steven Chavez
+//schavez256@yahoo.cnm.edu
+//BankAccount.h
+
+
+#ifndef _BANKACCOUNT_H
+#define _BANKACCOUNT_H
+
+#include <string>
+using namespace std;
+
+class BankAccount
+{
+
+private:
+	double balance;
+	double deposit;
+	double withdrawal;
+	string statusMsg;
+
+public:
+	BankAccount();  //default constructor
+	void SetDeposit(double dep);
+	void SetWithdrawal(double with);
+	double GetBalance(){return balance;}
+	string GetStatusMesg(){return statusMsg;}
+
+};
+
+#endif
